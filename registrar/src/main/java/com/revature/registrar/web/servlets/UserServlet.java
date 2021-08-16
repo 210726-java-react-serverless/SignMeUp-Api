8f158cfc2857f179871d4ad01833ebad6549acaf
ColbyDub <cduball@outlook.com>
2021-08-16 17:50:06 -0500

    test fix for git

--- registrar/src/main/java/com/revature/registrar/web/servlets/UserServlet.java
+++ registrar/src/main/java/com/revature/registrar/web/servlets/UserServlet.java
@@ -35,6 +35,7 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("test");
         System.out.println(req.getAttribute("filtered"));
         PrintWriter respWriter = resp.getWriter();
         resp.setContentType("application/json");
