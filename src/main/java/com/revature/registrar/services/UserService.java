383cf758c97439969b4729b48a4fc431d07446ea
raneyj <raney.jack99@gmail.com>
2021-08-18 10:25:17 -0500

    Checking for null password

--- src/main/java/com/revature/registrar/services/UserService.java
+++ src/main/java/com/revature/registrar/services/UserService.java
@@ -149,7 +149,9 @@ public class UserService {
         //TODO: Validation here
 
         //String encryptedPassword = passUtil.generateSecurePassword(password);
-
+        if(password == null) {
+            throw new InvalidRequestException("Bad pass");
+        }
         User user = userRepo.findUserByCredentials(username, password);
         setCurrUser(user);
         return user;
