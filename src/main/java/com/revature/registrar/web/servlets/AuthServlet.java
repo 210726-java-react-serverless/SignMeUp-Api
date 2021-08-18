3dd6e83e81e535ccef19eb039ec272f2f8676cb8
raneyj <raney.jack99@gmail.com>
2021-08-18 09:55:01 -0500

    sending stack trace

--- src/main/java/com/revature/registrar/web/servlets/AuthServlet.java
+++ src/main/java/com/revature/registrar/web/servlets/AuthServlet.java
@@ -47,10 +47,9 @@ public class AuthServlet extends HttpServlet {
             ErrorResponse errResp = new ErrorResponse(401, ae.getMessage());
             respWriter.write(mapper.writeValueAsString(errResp));
         }  catch (Exception e) {
-            e.printStackTrace();
             resp.setStatus(500); // server's fault
             ErrorResponse errResp = new ErrorResponse(500, "The server experienced an issue, please try again later.\n"
-            + e.getMessage());
+            + e.getStackTrace());
             respWriter.write(mapper.writeValueAsString(errResp));
         }
 
